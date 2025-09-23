1984aaa16f2d1d7eb98b7bff99dcbb3dd6e80ae4
axelldev <axellsolisrubio@gmail.com>
2025-09-22 23:41:51 -0600

    feat: implement categories filter

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,52 @@
 import ProductCard from "@/components/product/ProductCard";
+import { useCategories } from "@/hooks/useCategories";
 import { useProducts } from "@/hooks/useProducts";
+import { Colors } from "@/theme/colors";
 import { FlashList } from "@shopify/flash-list";
-import { StyleSheet, View } from "react-native";
+import { useMemo, useState } from "react";
+import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedCategory, setCategory] = useState("all");
+
   const { products, refetch, isRefetching } = useProducts();
+  const { categories } = useCategories();
+
+  const allCategories = useMemo(() => ["all", ...categories], [categories]);
+
+  const filteredProducts = useMemo(() => {
+    return products.filter((product) => {
+      if (selectedCategory !== "all") {
+        return product.category === selectedCategory;
+      }
+      return product.title.toLowerCase().includes(searchQuery.toLowerCase());
+    });
+  }, [selectedCategory, products, searchQuery]);
 
   return (
     <View style={styles.container}>
+      <View style={styles.categories}>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          {allCategories.map((category) => (
+            <Pressable
+              style={[
+                styles.category,
+                {
+                  backgroundColor:
+                    selectedCategory === category ? Colors.primary : "#f0f0f0",
+                },
+              ]}
+              key={category}
+              onPress={() => setCategory(category)}
+            >
+              <Text>{category}</Text>
+            </Pressable>
+          ))}
+        </ScrollView>
+      </View>
       <FlashList
-        data={products}
+        data={filteredProducts}
         renderItem={({ item }) => <ProductCard product={item} />}
         numColumns={2}
         contentContainerStyle={{ padding: 8 }}
@@ -24,5 +61,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+  },
+  categories: {
+    marginVertical: 8,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  category: {
+    padding: 8,
+    borderRadius: 8,
+    backgroundColor: "#f0f0f0",
+    marginHorizontal: 8,
   },
 });
--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,3 +1,5 @@
+import { BottomTabBarHeightCallbackContext } from "@react-navigation/bottom-tabs";
+
 export const Colors = {
   primary: "#007AFF",
 };
