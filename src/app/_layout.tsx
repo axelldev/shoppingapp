3baf591ea9adf6841117d921ca3f7bcada17f851
axelldev <axellsolisrubio@gmail.com>
2025-09-25 18:57:35 -0600

    feat: wrap Stack.Screen in Stack component for layout structure

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -5,7 +5,14 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <QueryClientProvider>
-      <Stack />
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            title: "Shop",
+          }}
+        />
+      </Stack>
     </QueryClientProvider>
   );
 }
